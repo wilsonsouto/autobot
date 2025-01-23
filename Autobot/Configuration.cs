06776ebcfaf3e755a5da7daf1a21986548db6a22
Wilson Souto <wilsonsouto@outlook.com.br>
2025-01-23 18:48:21 -0300

    Refactor Configuration class to search for multiple repositories dynamically

--- Autobot/Configuration.cs
+++ Autobot/Configuration.cs
@@ -2,22 +2,24 @@ namespace Autobot
 {
 	public static class Configuration
 	{
-		public static string ConsumerPath => Path.Combine(FindConsumerDirectory(), "src/");
+		public static string ConsumerPath => Path.Combine(FindDirectory("rogue-consumer-cob"), "src/");
 
-		public static string FindConsumerDirectory()
+		public static string DataPath => Path.Combine(FindDirectory("Data"));
+
+		public static string FindDirectory(string dir)
 		{
 			var currentDirectory = new DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory);
 
 			while (currentDirectory.Parent != null)
 			{
-				if (Directory.Exists(Path.Combine(currentDirectory.FullName, "rogue-consumer-cob")))
+				if (Directory.Exists(Path.Combine(currentDirectory.FullName, dir)))
 				{
-					return Path.Combine(currentDirectory.FullName, "rogue-consumer-cob");
+					return Path.Combine(currentDirectory.FullName, dir);
 				}
 				currentDirectory = currentDirectory.Parent;
 			}
 
-			throw new Exception("The directory 'rogue-consumer-cob' was not found");
+			throw new Exception($"O diretório {dir} não foi encontrado");
 		}
 	}
 
