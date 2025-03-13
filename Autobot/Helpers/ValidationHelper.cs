00044077c0715109a45c1e25f34982e33b0deaf4
Wilson Souto <wilsonsouto@outlook.com.br>
2025-03-13 10:57:48 -0300

    Fix increase valid string length in ValidationHelper

--- Autobot/Helpers/ValidationHelper.cs
+++ Autobot/Helpers/ValidationHelper.cs
@@ -2,6 +2,6 @@
 {
 	public static class ValidationHelper
 	{
-		public static bool IsStringValid(string? input) => !string.IsNullOrEmpty(input) && input.Length is >= 3 and <= 20;
+		public static bool IsStringValid(string? input) => !string.IsNullOrEmpty(input) && input.Length is >= 3 and <= 70;
 	}
 }
